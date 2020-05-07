06397f122650b0e6ffb390b070c1f472ec39a4c1
Anastasia <ejller@bk.ru>
2020-05-07 20:19:04 +0300

    add useList

--- src/pages/HomePage/rolePages/components/TableUsers.tsx
+++ src/pages/HomePage/rolePages/components/TableUsers.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useList } from "react-use";
 import _ from "lodash";
 
 import {
@@ -16,22 +16,31 @@ import {
 import { getUserList } from "../../../../data/fakeUserList";
 
 function TableUsers() {
-  const [users, setUsers] = useState(getUserList(5));
+  const [users, { updateAt, removeAt }] = useList(getUserList(15));
 
   const handleDisconnect = (n: number) => {
-    let tmp = Array.from(users);
-    tmp[n].status = {
-      info: "Offline",
-      isOnline: false,
-    };
-    setUsers(tmp);
+    let user = users[n];
+    updateAt(
+      n,
+      //It wants to take a full object from List
+      //I couldn't find a method that would allow to change
+      //the properties of the selected object and return it
+      {
+        login: user.login,
+        avatar: user.avatar,
+        role: user.role,
+        status: { info: "Offline", isOnline: false },
+      }
+      //Think, it's not very good to do this. Probably I use the method wrong.
+      //
+      //Object.defineProperty(users[n], "status", {
+      //value: { info: "Offline", isOnline: false },
+      //})
+    );
   };
 
-  //I don't sure but it looks better
   const handleDelete = (n: number) => {
-    let tmp = Array.from(users);
-    _.remove(tmp, tmp[n]);
-    setUsers(tmp);
+    removeAt(n);
   };
 
   const table = users.map((el, index) => (
